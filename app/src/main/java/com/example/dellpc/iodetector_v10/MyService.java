2a55384d93c8f903eb856301a5999c71159f91cc
Prerna237 <pr764@snu.edu.in>
2016-08-29 03:03:53 +0530

    Added magnetic declination

--- app/src/main/java/com/example/dellpc/iodetector_v10/MyService.java
+++ app/src/main/java/com/example/dellpc/iodetector_v10/MyService.java
@@ -1,10 +1,12 @@
 package com.example.dellpc.iodetector_v10;
 
+import android.Manifest;
 import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.hardware.GeomagneticField;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -16,34 +18,41 @@ import android.media.AudioRecord;
 import android.media.MediaRecorder;
 import android.os.BatteryManager;
 import android.os.Binder;
+import android.os.Bundle;
 import android.os.IBinder;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.telephony.PhoneStateListener;
 import android.telephony.SignalStrength;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.opencsv.CSVWriter;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class MyService extends Service {
-    public final String TAG="MyService";
+public class MyService extends Service implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+    public final String TAG = "MyService";
     private IBinder binder_service;
-    String[] headers_c1,headers_c2;
-    Thread t_css_1,t_li_1,t_pv_1,t_bt_2,t_sa_2,t_mv_2;
-    String[] csv_1=new String[4];
-    String[] csv_2=new String[3];
+    String[] headers_c1, headers_c2;
+    Thread t_css_1, t_li_1, t_pv_1, t_bt_2, t_sa_2, t_mv_2;
+    String[] csv_1 = new String[4];
+    String[] csv_2 = new String[3];
     String csv_file_name_1;
     String csv_file_name_2;
-    CSVWriter writer1= null;
+    CSVWriter writer1 = null;
     CSVWriter writer2 = null;
     GeomagneticField geoField;
     TelephonyManager mTelephonyManager;
     MyPhoneStateListener mPhoneStatelistener;
     int mSignalStrength = 0;
-    Boolean flag,flag0,flag1,flag2,flag3;
+    Boolean flag, flag0, flag1, flag2, flag3;
     SensorManager sensorManager;
     Sensor light_sensor;
     SensorEventListener lightEventListener;
@@ -54,6 +63,9 @@ public class MyService extends Service {
     float light_intensity_val;
     BroadcastReceiver mBatInfoReceiver;
     SoundMeter sm;
+    GoogleApiClient mGoogleApiClient;
+    Location mLocation;
+
     public MyService() {
     }
 
@@ -62,57 +74,60 @@ public class MyService extends Service {
         super.onCreate();
         //cell signal strength, light intensity, time of day and proximity value
         //battery temperature, sound amplitude  magnetic variance
-        sm=new SoundMeter();
-        csv_file_name_1 = android.os.Environment.getExternalStorageDirectory().getAbsolutePath()+"/class_1"+".csv";
-        csv_file_name_2 = android.os.Environment.getExternalStorageDirectory().getAbsolutePath()+"/class_2"+".csv";
-        headers_c1= new String[]{"Cell_Signal_Strength","Light_Intensity","Time","Proximity_val"};
-        headers_c2=new String[]{"Battery temp","sound_amp","magnetic_variance"};
+        sm = new SoundMeter();
+        mGoogleApiClient = build(mGoogleApiClient);
+        csv_file_name_1 = android.os.Environment.getExternalStorageDirectory().getAbsolutePath() + "/class_1" + ".csv";
+        csv_file_name_2 = android.os.Environment.getExternalStorageDirectory().getAbsolutePath() + "/class_2" + ".csv";
+        headers_c1 = new String[]{"Cell_Signal_Strength", "Light_Intensity", "Time", "Proximity_val"};
+        headers_c2 = new String[]{"Battery temp", "sound_amp", "magnetic_variance"};
         mPhoneStatelistener = new MyPhoneStateListener();
         mTelephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
         mTelephonyManager.listen(mPhoneStatelistener, PhoneStateListener.LISTEN_SIGNAL_STRENGTHS);
-        sensorManager=(SensorManager)getSystemService(Context.SENSOR_SERVICE);
-        light_sensor= sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-        prox_sensor=sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
-        mag_sensor=sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        light_sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        prox_sensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        mag_sensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
         Log.d(TAG, "Light Sensor:" + light_sensor);
         Log.d(TAG, "Prox Sensor:" + light_sensor);
 
     }
 
 
-
     @Override
     public IBinder onBind(Intent intent) {
         Log.d(TAG, "Inside onBind of service");
-        flag=false;flag0=false;flag1=false;flag2=false;flag3=false;
-        writer1=generateWriterHeadings(writer1,csv_file_name_1,headers_c1);
-        writer2=generateWriterHeadings(writer2,csv_file_name_2,headers_c2);
+        flag = false;
+        flag0 = false;
+        flag1 = false;
+        flag2 = false;
+        flag3 = false;
+        writer1 = generateWriterHeadings(writer1, csv_file_name_1, headers_c1);
+        writer2 = generateWriterHeadings(writer2, csv_file_name_2, headers_c2);
         sensorManager.registerListener(lightEventListener, light_sensor, SensorManager.SENSOR_DELAY_NORMAL);
         sensorManager.registerListener(proxEventListener, prox_sensor, SensorManager.SENSOR_DELAY_NORMAL);
-        t_css_1=new Thread(new Runnable() {
+        t_css_1 = new Thread(new Runnable() {
             @Override
             public void run() {
                 mPhoneStatelistener = new MyPhoneStateListener();
                 mTelephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
                 mTelephonyManager.listen(mPhoneStatelistener, PhoneStateListener.LISTEN_SIGNAL_STRENGTHS);
-                System.out.println("Signal Strength"+mSignalStrength);
-                csv_1[0]=Integer.toString(mSignalStrength);
-                flag0=true;
+                System.out.println("Signal Strength" + mSignalStrength);
+                csv_1[0] = Integer.toString(mSignalStrength);
+                flag0 = true;
             }
         });
-        t_li_1=new Thread(new Runnable() {
+        t_li_1 = new Thread(new Runnable() {
             @Override
             public void run() {
-                while(lightEventListener==null)
-                {
-                    lightEventListener=new SensorEventListener() {
+                while (lightEventListener == null) {
+                    lightEventListener = new SensorEventListener() {
                         @Override
                         public void onSensorChanged(SensorEvent event) {
-                            light_intensity_val=event.values[0];
-                            csv_1[1]=Float.toString(light_intensity_val);
-                            System.out.println("light:"+event.values[0]);
-                            flag1=true;
-                            sensorManager.unregisterListener(lightEventListener,light_sensor);
+                            light_intensity_val = event.values[0];
+                            csv_1[1] = Float.toString(light_intensity_val);
+                            System.out.println("light:" + event.values[0]);
+                            flag1 = true;
+                            sensorManager.unregisterListener(lightEventListener, light_sensor);
                         }
 
                         @Override
@@ -121,22 +136,21 @@ public class MyService extends Service {
                         }
                     };
                 }
-                sensorManager.registerListener(lightEventListener,light_sensor,SensorManager.SENSOR_DELAY_NORMAL);
+                sensorManager.registerListener(lightEventListener, light_sensor, SensorManager.SENSOR_DELAY_NORMAL);
             }
         });
 
-        t_pv_1=new Thread(new Runnable() {
+        t_pv_1 = new Thread(new Runnable() {
             @Override
             public void run() {
-                while(proxEventListener==null)
-                {
-                    proxEventListener=new SensorEventListener() {
+                while (proxEventListener == null) {
+                    proxEventListener = new SensorEventListener() {
                         @Override
                         public void onSensorChanged(SensorEvent event) {
-                            csv_1[3]=Float.toString(event.values[0]);
-                            System.out.println("prox:"+event.values[0]);
-                            sensorManager.unregisterListener(proxEventListener,prox_sensor);
-                            flag2=true;
+                            csv_1[3] = Float.toString(event.values[0]);
+                            System.out.println("prox:" + event.values[0]);
+                            sensorManager.unregisterListener(proxEventListener, prox_sensor);
+                            flag2 = true;
                         }
 
                         @Override
@@ -145,26 +159,27 @@ public class MyService extends Service {
                         }
                     };
                 }
-                sensorManager.registerListener(proxEventListener,prox_sensor,SensorManager.SENSOR_DELAY_NORMAL);
+                sensorManager.registerListener(proxEventListener, prox_sensor, SensorManager.SENSOR_DELAY_NORMAL);
             }
         });
 
-        mBatInfoReceiver = new BroadcastReceiver(){
+        mBatInfoReceiver = new BroadcastReceiver() {
             @Override
             public void onReceive(Context arg0, Intent intent) {
                 // TODO Auto-generated method stub
-                int temp = intent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE,0)/10;
-                System.out.println("battery"+temp);
-                csv_2[0]=Integer.toString(temp);
-                flag3=true;
+                int temp = intent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, 0) / 10;
+                System.out.println("battery" + temp);
+                csv_2[0] = Integer.toString(temp);
+                flag3 = true;
             }
         };
-        csv_2[1]=Double.toString(getAmp(sm)/10);
-        this.registerReceiver(this.mBatInfoReceiver,new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
-        csv_1[2]=""+(System.currentTimeMillis());
-        //csv_2[1]="audio";
-        csv_2[2]="location";
-        while(flag==false) {
+        this.registerReceiver(this.mBatInfoReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+        csv_2[1] = Double.toString(getAmp(sm) / 10);
+        csv_1[2] = "" + (System.currentTimeMillis());
+        mGoogleApiClient.connect();
+        geoField=new GeomagneticField((float)mLocation.getLatitude(),(float)mLocation.getLongitude(),(float) mLocation.getAltitude(),System.currentTimeMillis());
+        csv_2[2] =""+Float.toString(geoField.getDeclination());
+        while (flag == false) {
             if (flag1 == true && flag2 == true && flag3 == true && flag0 == true) {
                 flag = true;
             }
@@ -174,10 +189,9 @@ public class MyService extends Service {
 
     @Override
     public boolean onUnbind(Intent intent) {
-        while(true)
-        {
+        while (true) {
             System.out.println("Entered loop");
-            if (flag==true) {
+            if (flag == true) {
                 writer1.writeNext(csv_1);
                 try {
                     writer1.close();
@@ -194,25 +208,21 @@ public class MyService extends Service {
                 unregisterReceiver(mBatInfoReceiver);
                 System.out.println("Unbinding");
                 return super.onUnbind(intent);
-            }
-            else
-            {
+            } else {
                 ;
             }
         }
     }
 
-    private CSVWriter generateWriterHeadings(CSVWriter writer,String csv,String[] entries)
-    {
-        if(!new File(csv).exists())
-        {    //write title to csv file
+    private CSVWriter generateWriterHeadings(CSVWriter writer, String csv, String[] entries) {
+        if (!new File(csv).exists()) {    //write title to csv file
             try {
-                Log.d("","Created a new file");
+                Log.d("", "Created a new file");
                 writer = new CSVWriter(new FileWriter(csv, true), ',');
                 writer.writeNext(entries);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            catch (IOException e) {
-                e.printStackTrace();}
 
             try {
                 writer.close();
@@ -228,18 +238,54 @@ public class MyService extends Service {
             return null;
         }
     }
-    private void writeToCSV(String[] csvValues)
-    {}
 
-    private double getAmp(SoundMeter sm)
-    {
+    private void writeToCSV(String[] csvValues) {
+    }
+
+    private double getAmp(SoundMeter sm) {
         double amp;
         sm.start();
-        amp=sm.getAmplitude();
+        amp = sm.getAmplitude();
         sm.stop();
         return amp;
     }
 
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        System.err.println("Connection Suspended");
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        System.err.println("Connection Failed");
+    }
+
+    public GoogleApiClient build(GoogleApiClient mGoogleApiClient)
+    {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        return mGoogleApiClient;
+    }
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
     class MyPhoneStateListener extends PhoneStateListener {
 
         @Override
@@ -291,6 +337,7 @@ public class MyService extends Service {
         }
 
     }
+
 
 }
 
